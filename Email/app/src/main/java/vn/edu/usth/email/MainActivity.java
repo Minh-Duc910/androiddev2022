234a5e5a97261fac21cc1a8dd6ab6c49c521f375
Minh-Duc910 <Ducnm.bi10-031@st.usth.edu.vn>
2021-10-14 20:28:28 +0700

    email

---
+++ Email/app/src/main/java/vn/edu/usth/email/MainActivity.java
@@ -0,0 +1,24 @@
+package vn.edu.usth.email;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onShowInbox(View view){
+        Intent intent = new Intent(this,InboxActivity.class);
+        startActivity(intent);
+    }
+    public void onShowSent(View view){
+        Intent intent = new Intent(this, SentActivity.class);
+        startActivity(intent);
+    }
+}
