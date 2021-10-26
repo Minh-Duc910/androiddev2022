533a07edb4a20b9d455cbb54b5ec63e73e72a6c1
Minh-Duc910 <Ducnm.bi10-031@st.usth.edu.vn>
2021-10-26 16:28:52 +0700

    practice 8

--- USTHWeather2/app/src/main/java/vn/edu/usth/usthweather2/MainActivity.java
+++ USTHWeather2/app/src/main/java/vn/edu/usth/usthweather2/MainActivity.java
@@ -1,13 +1,18 @@
 package vn.edu.usth.usthweather2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +28,29 @@ public class MainActivity extends AppCompatActivity {
                 new SectionsPagerAdapter(getSupportFragmentManager());
         ViewPager pager = (ViewPager) findViewById(R.id.pager);
         pager.setAdapter(pagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(pager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate menu, add items to the app bar
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.refresh:
+                //refresh when button is clicked
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private class SectionsPagerAdapter extends FragmentPagerAdapter {
@@ -45,5 +73,19 @@ public class MainActivity extends AppCompatActivity {
             }
             return null;
         }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return getResources().getText(R.string.home_tab);
+                case 1:
+                    return getResources().getText(R.string.still_home);
+                case 2:
+                    return getResources().getText(R.string.like_home);
+            }
+            return null;
+        }
     }
+
 }
